5c0b3c3c13e420d8c0d1eb27bb22d1dc2274fb63
estebanhirzfeld <estebanhirzfeld@gmail.com>
2021-11-08 03:05:18 -0300

    Primer Commit y Subida de Archivos

---
+++ javascript/app.js
@@ -0,0 +1,38 @@
+
+// Instanciacion de Videos
+
+const URLJSON = "/assets/JSON/videos.json"
+
+GetVideos();
+
+function GetVideos() {
+    $.getJSON(URLJSON, function (respuesta, estado) {
+        if (estado == "success") {
+            let videos = respuesta;
+            // videos = videos.sort(() => Math.random() -0.5) random sort videos
+            for (const video of videos){
+                $(".videoGrid").append(
+                    `
+                    <div id="${video.id}" class="col-6 col-md-4 col-lg-3 d-flex flex-column justify-content-center monroeVideo"><img class="videoThumbnail" src="https://drive.google.com/thumbnail?id=${video.videoId}" alt=""> <text>${video.nombre}</text> <div class="d-flex align-items-center mb-3"> <ion-icon name="checkmark-circle"></ion-icon> <span>${video.prod}</span></div> </div>
+
+                    `
+                    )
+            }
+            
+
+        }
+    })
+}
+
+
+$(".videoThumbnail").click(function(){
+    console.log("sexo")
+})
+
+
+
+
+// $(".videoThumbnail").click(function(){
+//     console.log(${video.nombre})
+// })
+
---
+++ javascript/video-play.js
@@ -0,0 +1,17 @@
+// $(".btnEncontrarOfertaMS").click(function(){
+//     window.location.replace("../pages/explorar.html");
+// })
+
+// GetVideoToPlay();
+
+$(".divVideoPlay").append(
+    `
+    <iframe class="col-8 col-md-8 col-lg-8" src="https://drive.google.com/file/d/1QDrLRkzsH5IpJCcTJIyyHptr5eviuolU/preview" width="640" height="480" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+    
+    <div class="col-3 col-md-3 col-lg-3">1</div>
+    <div class="col-11 col-md-11 col-lg-11 mx-3">
+        <span></span>
+    </div>
+    `
+)
+
